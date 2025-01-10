735621be2c29b8b9ab5f268ea9bb279ab2bfe95a
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2025-01-10 09:55:42 +0630

    feat: add CORS policy

--- RetailAPI/Program.cs
+++ RetailAPI/Program.cs
@@ -5,6 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 var appSettings = new AppSettings();
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // Replace with your front-end URL
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +24,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
+
+// Enable the CORS policy globally
+app.UseCors("AllowSpecificOrigin");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
